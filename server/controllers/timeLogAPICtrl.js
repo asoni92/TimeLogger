d65c70cb4f9d8f5e7f3c5757a618a489cde56cd0
Infoobjects <infoobjects@Akshay-MacBook.local>
2017-09-05 02:06:14 +0530

    Add functionality // BaseModel Add

--- server/controllers/timeLogAPICtrl.js
+++ server/controllers/timeLogAPICtrl.js
@@ -85,44 +85,100 @@ module.exports = function(app) {
 		var username = req.body.user_name;
 		var command = req.body.text;
 		if (command == null || command == '' || command.trim().length == 0) {
-			//Handle the unwanted exception
-			console.log("---------If--------------")
+			// Handle the unwanted exception
 			return res.send(welcomeMsg);
 		}
-		var code = null;
-		var description = null;
-		var option = null;
-		var descriptionIndex = command.indexOf("'");
 		var data = command.split("--");
 		console.log(data);
+		var option = data[1];
 		if (data.length == 0) {
-			//Handle the error
+			// Handle the error
 		}
-		console.log('--['+data[1]+']--')
-		if (data[1].toLowerCase() == 'add') {
+		console.log('--['+data[2]+']--')
+		console.log('--['+data[3]+']--')
+		if (option.toLowerCase() == 'add') {
 			// Do the add operation
-			console.log('Add operation Called')
-			addLog(req, res, next);
-		} else if (data[1].toLowerCase() == 'update') {
-			//do the update operation 
-		} else if (data[1].toLowerCase() == 'logs') {
-			//do the logs operation
-		} else if (data[1].toLowerCase() == 'remove') {
+			var workLogTime = getTimeFromRequest(data[2]);
+			var description = getDescriptionFromRequest(data[3])
+			if(workLogTime == null) {
+				return res.send(welcomeMsg);
+			}
+			if(description == null) {
+				return res.send(welcomeMsg);
+			}
+			addLog(req, res, workLogTime,description);
+		} else if (option.toLowerCase() == 'update') {
+			// do the update operation
+		} else if (option.toLowerCase() == 'logs') {
+			// do the logs operation
+		} else if (option.toLowerCase() == 'remove') {
 			// do the remove operation
 		}
 		return res.send(command);
 	};
 	
-	function addLog(req, res, next) {
+	function getDescriptionFromRequest(data) {
+		var description = null;
+		if(data == null || data == '' || data.trim().length == 0) {
+			return null;
+		} else {
+			data = data.trim();
+			if(data.startsWith("'") && data.endsWith("'")) {
+				description = data.substring(1, data.length - 1);
+			}
+			return description;
+		}
+	}
+	
+	function getTimeFromRequest(data) {
+		var time = null;
+		if(data == null || data == '' || data.trim().length == 0) {
+			return null;
+		} else {
+			data = data.trim();
+			if(data.toLowerCase.endsWith('h')) {
+				var temp = data.substring(0,data.lastIndexOf('h'));
+			    if(temp == null || temp == '' || isNaN(temp)) {
+			    	// Invalid time
+			    	return time;
+			    } else {
+			    	temp = parseFloat(temp);
+			    	time = temp * 60 * 60;
+			    }
+			} else if(data.toLowerCase.endsWith('m')) {
+				var temp = data.substring(0,data.lastIndexOf('m'));
+			    if(temp == null || temp == '' || isNaN(temp)) {
+			    	// Invalid time
+			    	return time;
+			    } else {
+			    	temp = parseFloat(temp);
+			    	time = temp * 60;
+			    }
+			} else if(data.toLowerCase.endsWith('s')) {
+				var temp = data.substring(0,data.lastIndexOf('m'));
+			    if(temp == null || temp == '' || isNaN(temp)) {
+			    	// Invalid time
+			    	return time;
+			    } else {
+			    	temp = parseFloat(temp);
+			    	time = temp;
+			    }
+			}  
+			return time;
+		}
+	}
+	
+	function addLog(req, res, workLogTime, description) {
 		try {
 		var now = moment().unix();
 		var _workLog = {
 				code: uniqid(),
 				userId: 1,
-				time: 10,
+				time: workLogTime,
 				dateCreated: now,
 				dateModified: now,
-				active: true
+				active: true,
+				description: description
 			}
 			schema.model('WorkLog').forge().save(_workLog).then(function (savedWork) {
 				console.log("Saved Work is ");
