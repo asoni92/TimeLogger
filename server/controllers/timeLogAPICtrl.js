598d74d3cf9d670dff880de2055d23155bbfdf13
Infoobjects <infoobjects@Akshay-MacBook.local>
2017-09-05 05:27:49 +0530

    Added Welcome Message

--- server/controllers/timeLogAPICtrl.js
+++ server/controllers/timeLogAPICtrl.js
@@ -125,7 +125,7 @@ module.exports = function(app) {
 			}).fetch().then(function (result) {
 				if (result) {
 					var userData = result.toJSON();
-					req.headers.user_id = userData.get('id');
+					req.headers.user_id = result.get('id');
 					console.log('User id is '+req.headers.user_id)
 					processRequest(req, res, next);
 				} else {
