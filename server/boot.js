b9fa9fc45108bf77e6ee5249fca07d6766b814c3
Infoobjects <infoobjects@Akshay-MacBook.local>
2017-09-05 05:05:42 +0530

    Add functionality User Id

--- server/boot.js
+++ server/boot.js
@@ -64,7 +64,7 @@ module.exports = function (app, cb) {
 
     app.use('/services', express);
     app.use(function (req, res, next) {
-    	 if (!req.headers["access-control-request-method"] && && req.url.indexOf('/api/processRequest') >= 0) {
+    	 if (!req.headers["access-control-request-method"] && req.url.indexOf('/api/processRequest') >= 0) {
     		 populateUserId(req, res, next);
          } else {
              handleRequest(req, res, next);
